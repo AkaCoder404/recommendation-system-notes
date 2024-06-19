aa2adc869ea27d975ae593639e27b518f055933b
AkaCoder404 <akacoder404@outlook.com>
2024-06-19 16:46:54 +0800

    update notes

---
+++ hysage-algo/movielens100k.py
@@ -0,0 +1,7 @@
+"""
+
+Title: Hybrid SageMaker Algorithm for MovieLens 100k
+
+Description: Implementation of HySAGE algorithm for MovieLens 100k dataset
+"""
+
---
+++ knn-algo/knnbasic_movielens100k.py
@@ -0,0 +1,5 @@
+"""
+Title: KNN Basic for MovieLens 100k
+
+Description: Implementation of KNN Basic algorithm for MovieLens 100k dataset
+"""
